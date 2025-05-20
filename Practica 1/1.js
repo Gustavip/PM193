f6aba268fc473ab2cf9a580c7f46184828494d66
Gustavip <148286101+Gustavip@users.noreply.github.com>
2025-05-20 02:42:57 -0600

    Reemplazar var por let

---
+++ Practica 1/1.js
@@ -0,0 +1,8 @@
+let nombre = "Armando"; // Se puede reasignar después
+const edad = 25; // No se reasigna, por eso usamos const
+
+nombre = "Ana Maria"; // Reasignación permitida porque usamos let
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+console.log(saludo); // Muestra en consola el contenido de saludo
