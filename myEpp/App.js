e1ce6c837714fd8a6fb8a75604624dd400d02690
Gustavip <148286101+Gustavip@users.noreply.github.com>
2025-06-10 22:07:19 -0600

    Props y State

--- myEpp/App.js
+++ myEpp/App.js
@@ -1,15 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+// Zona 1 : lugar de las importaciones 
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+// Componente personalizado para mostrar texto
+const Texto = (props) => {
+  const { contenido } = props
+  return (<Text>{contenido}</Text>)
+  
+}
+// zona 2 main 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola" />
+      <Texto contenido="mundo" />
+      <Texto contenido="React Native" />
+      <Button title="Precionar"> </Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Zona 3 : de estilos 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
