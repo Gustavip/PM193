fb4939cfc7ee350233e6737290f079303dc2796f
Gustavip <148286101+Gustavip@users.noreply.github.com>
2025-06-12 17:22:02 -0600

    modificacion de Button

--- myEpp/App.js
+++ myEpp/App.js
@@ -13,16 +13,22 @@ const Texto = () => {
   const actualizaTexto=()=>{setContenido('Estado modificado')}
   return(
   <Text onPress={actualizaTexto}> {contenido} </Text>
+  
+
   )
 }
+
+
 // zona 2 main 
 export default function App() {
+  const [contenidoBoton, setContenidoBoton] = useState('Presionar');
+  const actualizaTexto = () => { setContenidoBoton('Ubu'); }
   return (
     <View style={styles.container}>
       <Texto></Texto>  
       <Texto></Texto>
       <Texto></Texto>
-      <Button title="Precionar"> </Button>
+      <Button title={contenidoBoton} onPress={actualizaTexto} />
       <StatusBar style="auto" />
     </View>
   );
