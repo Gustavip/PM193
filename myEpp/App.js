669b0f6aeb2dd2b7f81a265d67745e2bb3450ffe
Gustavip <148286101+Gustavip@users.noreply.github.com>
2025-06-16 20:26:12 -0600

    estilos

--- myEpp/App.js
+++ myEpp/App.js
@@ -8,16 +8,15 @@ import React,{useState} from 'react';
 
 
 // Componente personalizado para mostrar texto
-const Texto = () => {
+const Texto = ({style}) => {
   const [contenido,setContenido]=useState('Hola mundo')
   const actualizaTexto=()=>{setContenido('Estado modificado')}
   return(
-  <Text onPress={actualizaTexto}> {contenido} </Text>
+  <Text  style={[styles.text,style]} onPress={actualizaTexto}> {contenido} </Text> //aqui declaramos que solo texto sera modifado con los estilos 
   
-
+  
   )
 }
-
 
 // zona 2 main 
 export default function App() {
@@ -25,10 +24,11 @@ export default function App() {
   const actualizaTexto = () => { setContenidoBoton('Ubu'); }
   return (
     <View style={styles.container}>
-      <Texto></Texto>  
-      <Texto></Texto>
-      <Texto></Texto>
-      <Button title={contenidoBoton} onPress={actualizaTexto} />
+
+      <Texto style={styles.blue}> </Texto>  
+      <Texto style={styles.red}> </Texto>
+      <Texto style={styles.green}> </Texto>
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -39,7 +39,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'strech', //de izquierda a derecha 
+    justifyContent: 'center', // de ariba a bajo 
+  
   },
+  text:{                               // agregamos los estilos del texto 
+    color:'black',                      // agregamosm una clase
+    fontsize:27,
+   // height:120,
+   // width:120,
+  },
+  blue:{backgroundColor:'blue'},
+  red:{backgroundColor: 'red'},
+  green:{backgroundColor:'green'},
+
 });
